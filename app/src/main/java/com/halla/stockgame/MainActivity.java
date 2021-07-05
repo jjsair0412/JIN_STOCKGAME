5c0ac90529edf6acc1cbd9d90b5c5fdb2a520e77
jjsair0412 <jjsair0412@naver.com>
2021-07-05 10:34:45 +0900

    commit

--- app/src/main/java/com/halla/stockgame/MainActivity.java
+++ app/src/main/java/com/halla/stockgame/MainActivity.java
@@ -1,8 +1,10 @@
 package com.halla.stockgame;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,6 +13,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -73,7 +76,18 @@ public class MainActivity extends AppCompatActivity {
             HelpBtn.setOnClickListener(new View.OnClickListener() { //도움말 버튼
                 @Override
                 public void onClick(View view) {
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    dlg.setTitle("모의 주식 투자");
+                    dlg.setMessage("가상의 종목들을 활용해, 주식 투자를 체험해보세요!" + "\n"+"투자금액은 만원으로 모두 동일해요"+"\n"+"투자금을 모두 잃었다면, 리필버튼을 클릭해 보세요!");
+                    dlg.setIcon(R.drawable.ic_launcher_background);
+                    dlg.setPositiveButton("돌아가기", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Toast.makeText(MainActivity.this,"게임을 즐겨보세요.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
+                    dlg.show();
                 }
             });
 
@@ -82,7 +96,9 @@ public class MainActivity extends AppCompatActivity {
             QuitBtn.setOnClickListener(new View.OnClickListener() { // 게임종료 버튼
                 @Override
                 public void onClick(View view) {
-
+                    finishAffinity();
+                    System.runFinalization();
+                    System.exit(0);
                 }
             });
 
--- app/src/main/java/com/halla/stockgame/StockListFragment.java
+++ app/src/main/java/com/halla/stockgame/StockListFragment.java
@@ -1,4 +1,4 @@
-package com.halla.stockgame;
+    package com.halla.stockgame;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,8 +33,6 @@ public class StockListFragment extends Fragment {
 
     private Thread thread;
 
-    String stock_name; //서버로가는 종목이름
-    String stock_price; //서버로가는 종목가격
 
     String stockname; //리스트뷰에 뿌려줄 종목이름
     String stockprice; //리스트뷰에 뿌려줄 종목가격
